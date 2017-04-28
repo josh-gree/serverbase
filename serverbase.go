205e68b377d2ad321adfacd0c9d1273e08acf448
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2017-04-28 18:57:09 +0100

    Some change...

--- serverbase.go
+++ serverbase.go
@@ -2,7 +2,10 @@ package serverbase
 
 import (
 	"fmt"
+	"encoding/json"
 	"github.com/labstack/echo"
+	"net/http"
+	"bytes"
 )
 
 type Message struct {
@@ -11,10 +14,10 @@ type Message struct {
 }
 
 type Go struct {
-	Go bool `json:"go"`
+	Time float64 `json:"time"`
 }
 
-func Start(c echo.Context) error{
+func StartP(c echo.Context) error{
 	g := Go{}
 
 	err := c.Bind(&g)
@@ -28,7 +31,34 @@ func Start(c echo.Context) error{
 	return nil
 }
 
-func Get(c echo.Context) error{
+func Send(m Message) error{
+	// data, err := json.Marshal(m)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return err
+	// }
+	//
+	// fmt.Println("JSON to send;")
+	// fmt.Println(string(data))
+
+	g := Go{
+		Time : m.Time,
+	}
+	data, err := json.Marshal(g)
+
+	fmt.Println("JSON to send;")
+	fmt.Println(string(data))
+
+	resp, err := http.Post(fmt.Sprintf("http://localhost:%d/start",m.Dest),"application/json",bytes.NewBuffer(data))
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	fmt.Println(resp)
+	return nil
+}
+
+func GetP(c echo.Context) error{
 	m := Message{}
 
 	err := c.Bind(&m)
@@ -37,14 +67,22 @@ func Get(c echo.Context) error{
 		return err
 	}
 
+	Send(m)
+
+
 	fmt.Printf("%#v\n",m)
 
 	return nil
 }
-func Listen(port int){
+func Listen(master bool){
 		e := echo.New()
-		fmt.Printf("Starting to listen on %d",port)
-		e.POST("/start", Start)
-		e.POST("/msg", Get)
-		e.Start(fmt.Sprintf(":%d",port))
+		fmt.Printf("Starting to listen on %d\n",8000)
+		if master {
+			fmt.Println("Master process")
+		} else {
+			fmt.Println("Slave process")
+		}
+		e.POST("/start", StartP)
+		e.POST("/msg", GetP)
+		e.Start(":8000")
 }
